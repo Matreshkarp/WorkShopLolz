ff27b325eef1baf416ec8daa4c04ebe81ac6b24d
Matreshkarp <94852791+Matreshkarp@users.noreply.github.com>
2022-02-26 14:15:49 +0300

    Add files via upload

---
+++ configure.py
@@ -0,0 +1,6 @@
+config = {
+	'WorkShopLolz_bot': '',#СЛИТО В ТГ КАНАЛЕ @END_RAID
+	'5225875029:AAH9_PWO-4VaGGMf76ZlPbC66XeAYTgQ8R4': '',#СЛИТО В ТГ КАНАЛЕ @END_RAID
+	'none': '',#СЛИТО В ТГ КАНАЛЕ @END_RAID
+	'79885664132': ''#СЛИТО В ТГ КАНАЛЕ @END_RAID
+}
